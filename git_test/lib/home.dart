6a0ddc0ce9b6aaa5e09f88de640a51add653b395
nnmmnn15 <shals1645@naver.com>
2024-07-24 18:51:47 +0900

    setting

---
+++ git_test/lib/home.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('현대자동차'),
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+      ),
+      body: Column(
+        
+      ),
+    );
+  }
+}
