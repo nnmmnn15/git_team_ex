836bb6178b2c2c934469187783da541246260626
nnmmnn15 <shals1645@naver.com>
2024-07-25 09:57:59 +0900

    home btn

--- git_test/lib/home.dart
+++ git_test/lib/home.dart
@@ -6,7 +6,29 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () => Navigator.pushNamed(context,'/1'),
+              child: const Text('1'),
+            ),
+            ElevatedButton(
+              onPressed: () => Navigator.pushNamed(context,'/2'),
+              child: const Text('2'),
+            ),
+            ElevatedButton(
+              onPressed: () => Navigator.pushNamed(context,'/3'),
+              child: const Text('3'),
+            ),
+            ElevatedButton(
+              onPressed: () => Navigator.pushNamed(context,'/4'),
+              child: const Text('4'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
